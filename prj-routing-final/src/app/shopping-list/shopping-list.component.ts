e311b111bc5980bb8f7c5a8548fc54ad4ab8e30b
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-08-04 12:18:54 -0400

    commit

--- prj-routing-final/src/app/shopping-list/shopping-list.component.ts
+++ prj-routing-final/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subscription } from "rxjs";
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -10,12 +10,12 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
-
+  igChangeSub: Subscription;
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged
+    this.igChangeSub = this.slService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
